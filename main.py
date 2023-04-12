44f47c71d75c53a16bf6744b14a642d8fbc6ec77
alexlpazMphg <alex@mphg.com.br>
2023-04-12 17:45:39 -0300

    Estrutura API

---
+++ main.py
@@ -0,0 +1,82 @@
+import flask
+from src.winePrj import GetAllInTable
+from src.winePrj import InsertInTable
+from src.winePrj import GetInTable
+from src.winePrj import GetInTableAddQuery
+from src.winePrj import UpdateInTable
+from src.winePrj import DeleteInTable
+from src.winePrj import UrlParser
+from src.winePrj import CreateConnection
+
+app = flask.Flask(__name__)
+
+
+@app.route('/', defaults={'path': ''}, methods=['POST', 'GET', 'PUT', 'DELETE'])
+@app.route('/<path:path>', methods=['POST', 'GET', 'PUT', 'DELETE'])
+def process_request(path):
+    connection = CreateConnection().run()
+    cursor = connection.cursor()
+    vars_return = UrlParser().run(flask.request.url, cursor)
+    if vars_return is None:
+        result = {'code': 501,
+                  'name': 'Not Implemented',
+                  'description': 'Not allowed - Invalid endpoint'
+                  }
+        return result, 501
+    else:
+        table_from_url = vars_return[0][0]
+        if len(vars_return[0]) > 1:
+            zid_from_url = vars_return[0][1]
+        else:
+            zid_from_url = None
+        if len(vars_return) > 1:
+            zquery_from_url = vars_return[1]
+        else:
+            zquery_from_url = None
+        method = flask.request.method
+        if method == 'POST':
+            if zid_from_url is not None:
+                result = {'code': 501,
+                          'name': 'Not Implemented',
+                          'description': 'POST method not allowed with parameters in URL'
+                          }
+                return result, 501
+            else:
+                new_item_json = flask.request.get_json()
+                new_item = InsertInTable().run(table_from_url, new_item_json, cursor, connection)
+                return new_item
+        elif method == 'GET':
+            if zquery_from_url is not None:
+                item = GetInTableAddQuery().run(table_from_url, zquery_from_url, cursor)
+                return flask.jsonify(item)
+            elif zid_from_url is not None:
+                item = GetInTable().run(table_from_url, zid_from_url, cursor)
+                return flask.jsonify(item)
+            else:
+                items = GetAllInTable().run(table_from_url, cursor)
+                return flask.jsonify(items)
+        elif method == 'PUT':
+            if zid_from_url is None:
+                result = {'code': 501,
+                          'name': 'Not Implemented',
+                          'description': 'PUT method not allowed with no parameters in URL'
+                          }
+                return result, 501
+            else:
+                update_item_json = flask.request.get_json()
+                item_update = UpdateInTable().run(table_from_url, vars_return[1], update_item_json, cursor, connection)
+                return item_update
+        else:
+            if zid_from_url is None:
+                result = {'code': 501,
+                          'name': 'Not Implemented',
+                          'description': 'DELETE method not allowed with no parameters in URL'
+                          }
+                return result, 501
+            else:
+                item_delete = DeleteInTable().run(table_from_url, zid_from_url, cursor, connection)
+                return item_delete
+    connection.close()
+
+
+app.run(port=5000, host='localhost', debug=True)
---
+++ src/winePrj.py
@@ -0,0 +1,124 @@
+import mysql.connector
+from urllib.parse import urlparse, unquote
+
+
+class CreateConnection:
+    def run(self):
+        try:
+            conn = mysql.connector.connect(host='localhost', database='wine_4all', user='alexlpaz', password='Paulista@123')
+            return conn
+        except mysql.connector.Error as e:
+            print('Error while connecting to MySQL: %s', e)
+
+
+class InsertInTable:
+    def run(self, table_from_url, json_str, cursor, connection):
+        values = [list(x.values()) for x in json_str]
+        columns = [list(x.keys()) for x in json_str][0]
+        values_str = ""
+        for i, record in enumerate(values):
+            val_list = []
+            for v, val in enumerate(record):
+                if type(val) == str:
+                    val = "'{}'".format(val.replace("'", "''"))
+                val_list += [str(val)]
+            values_str += "(" + ', '.join(val_list) + "),\n"
+        values_str = values_str[:-2] + ";"
+        sql_string = "INSERT INTO %s (%s)\nVALUES\n%s" % (
+            table_from_url,
+            ', '.join(columns),
+            values_str
+        )
+        cursor.execute(sql_string)
+        connection.commit()
+        rows = cursor.rowcount
+        return 'Success: ' + str(rows) + ' rows inserted in table ' + str(table_from_url)
+
+
+class GetInTable:
+    def run(self, table_from_url, id, cursor):
+        sql_string = "SELECT * FROM " + str(table_from_url) + " WHERE id = %s"
+        cursor.execute(sql_string, (id,))
+        record = cursor.fetchall()
+        row_headers = [x[0] for x in cursor.description]
+        json_data = []
+        for result in record:
+            json_data.append(dict(zip(row_headers, result)))
+        return json_data
+
+class GetInTableAddQuery:
+    def run(self, table_from_url, add_query, cursor):
+        sql_string = "SELECT * FROM " + str(table_from_url) + " WHERE " + add_query
+        cursor.execute(sql_string)
+        record = cursor.fetchall()
+        row_headers = [x[0] for x in cursor.description]
+        json_data = []
+        for result in record:
+            json_data.append(dict(zip(row_headers, result)))
+        return json_data
+
+class GetAllInTable:
+    def run(self, table_from_url, cursor):
+        sql_string = "SELECT * FROM " + str(table_from_url)
+        cursor.execute(sql_string)
+        record = cursor.fetchall()
+        row_headers = [x[0] for x in cursor.description]
+        json_data = []
+        for result in record:
+            json_data.append(dict(zip(row_headers, result)))
+        return json_data
+
+
+class UpdateInTable:
+    def run(self, table_from_url, id, json_str, cursor, connection):
+        values = [list(x.values()) for x in json_str]
+        columns = [list(x.keys()) for x in json_str][0]
+        values_str = ""
+        for i, record in enumerate(values):
+            val_list = []
+            for v, val in enumerate(record):
+                if type(val) == str:
+                    val = columns[v] + "='" + val + "'"
+                val_list += [str(val)]
+            values_str += "" + ', '.join(val_list) + "),"
+        values_str = values_str[:-2]
+        sql_string = "UPDATE %s SET %s WHERE id = %s" % (
+            table_from_url,
+            values_str, id
+        )
+        cursor.execute(sql_string)
+        connection.commit()
+        rows = cursor.rowcount
+        cursor.close()
+        connection.close()
+        return 'Success: ' + str(rows) + ' rows updated in table ' + str(table_from_url)
+
+
+class DeleteInTable:
+    def run(self, table_from_url, id, cursor, connection):
+        sql_string = "DELETE FROM " + str(table_from_url) + " WHERE id = %s"
+        cursor.execute(sql_string, (id,))
+        connection.commit()
+        rows = cursor.rowcount
+        cursor.close()
+        connection.close()
+        return 'Success: ' + str(rows) + ' rows deleted in table ' + str(table_from_url)
+
+
+class UrlParser:
+    def run(self, base_url, cursor):
+        parts = urlparse(base_url)
+        directories = parts.path.strip('/').split('/')
+        cursor.execute("SHOW TABLES")
+        table_return = ""
+        for table_name in cursor:
+            if " | ".join(table_name) == directories[0]:
+                table_return = directories[0]
+        if table_return == "":
+            return None
+        else:
+            return_to_func = [directories]
+            add_query = unquote(parts.query)
+            if len(add_query) > 0:
+                return_to_func.append(add_query)
+            return return_to_func
